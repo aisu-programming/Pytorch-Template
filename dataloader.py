214477d3cbf4a825bc6de4e67f5d40de9e72d8fb
aisu-programming <aisu.programming@gmail.com>
2022-10-21 13:45:24 +0800

    Add dataloader

---
+++ dataloader.py
@@ -0,0 +1,53 @@
+""" Libraries """
+import math
+import torch
+import torch.utils.data
+from typing import Any, Iterator, Tuple
+from torch.utils.data import Dataset, IterableDataset, Subset
+
+
+
+""" Classes """
+class MyMapDataset(Dataset):
+    def __init__(self, data) -> None:
+        super(MyMapDataset).__init__()
+        self.data = data
+
+    def __getitem__(self, index: Any) -> Any:
+        return self.data[index]
+
+    def __len__(self) -> int:
+        raise len(self.data)
+
+
+class MyIterableDataset(IterableDataset):
+    def __init__(self, start: int, end: int) -> None:
+        super(MyIterableDataset).__init__()
+        assert end > start, "this example code only works with end >= start"
+        self.start = start
+        self.end = end
+        
+    def __iter__(self) -> Iterator[int]:
+        worker_info = torch.utils.data.get_worker_info()
+        if worker_info is None:
+            iter_start = self.start
+            iter_end = self.end
+        else:
+            per_worker = int(math.ceil((self.end - self.start) / float(worker_info.num_workers)))
+            worker_id = worker_info.id
+            iter_start = self.start + worker_id * per_worker
+            iter_end = min(iter_start + per_worker, self.end)
+        return iter(range(iter_start, iter_end))
+
+
+
+""" Functions """
+def split_datasets(dataset: Dataset) -> Tuple[Subset, Subset]:
+    dataset_length = len(dataset)
+    train_dataset_length = int(dataset_length*0.8)
+    valid_dataset_length = dataset_length - train_dataset_length
+    train_dataset, valid_dataset = \
+        torch.utils.data.random_split(
+            dataset, [train_dataset_length, valid_dataset_length],
+            generator=torch.Generator().manual_seed(0))
+    return train_dataset, valid_dataset
--- train.py
+++ train.py
@@ -4,40 +4,75 @@ import time
 import shutil
 import argparse
 from tqdm import tqdm
+from torch.utils.data import DataLoader
+
+from dataloader import MyMapDataset, split_datasets
 
 
 
 """ Functions """
-def train_step():
-    raise NotImplementedError
+def train_step(batch_data):
+    pass
 
 
-def valid_step():
-    raise NotImplementedError
+def valid_step(batch_data):
+    pass
 
 
-def train_epoch():
-    raise NotImplementedError
+def train_epoch(dataloader):
+    pbar = tqdm(enumerate(dataloader), ascii=True, desc="[TRAIN]")
+    for batch_i, (batch_data) in pbar:
+        train_step(batch_data)
+    return
 
 
-def valid_epoch():
-    raise NotImplementedError
+def valid_epoch(dataloader):
+    pbar = tqdm(enumerate(dataloader), ascii=True, desc="[VALID]")
+    for batch_i, (batch_data) in pbar:
+        valid_step(batch_data)
+    return
 
 
 def main(args):
     os.makedirs(args.save_dir, exist_ok=True)
     shutil.copy(__file__, args.save_dir)
+    shutil.copy("models.py", args.save_dir)
+    shutil.copy("dataloader.py", args.save_dir)
+    my_data = list(range(100))
+    my_dataset = MyMapDataset(my_data)
+    my_train_dataset, my_valid_dataset = split_datasets(my_dataset)
+    my_train_dataLoader = DataLoader(
+        my_train_dataset, args.batch_size, shuffle=True, sampler=None,
+        batch_sampler=None, num_workers=args.num_workers, collate_fn=None,
+        pin_memory=True, drop_last=False, timeout=0, worker_init_fn=None,
+        multiprocessing_context=None, generator=None,
+        prefetch_factor=2, persistent_workers=False
+    )
+    my_valid_dataLoader = DataLoader(
+        my_valid_dataset, args.batch_size, shuffle=True, sampler=None,
+        batch_sampler=None, num_workers=args.num_workers, collate_fn=None,
+        pin_memory=True, drop_last=False, timeout=0, worker_init_fn=None,
+        multiprocessing_context=None, generator=None,
+        prefetch_factor=2, persistent_workers=False
+    )
+    for epoch in range(1, args.epochs+1):
+        print(f"{epoch}/{args.epochs}")
+        train_epoch(my_train_dataLoader)
+        valid_epoch(my_valid_dataLoader)
     return
 
 
 
 """ Execution """
 if __name__ == "__main__":
+
+    DEFAULT_SAVE_DIR = f"logs/{time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime())}"
+
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--save-dir", type=str,
-        default=f"logs/{time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime())}",
-        help="path of directory to save log files",
-    )
+    parser.add_argument("-e",  "--epochs",      type=int, default=100)
+    parser.add_argument("-bs", "--batch-size",  type=int, default=32)
+    parser.add_argument("-nw", "--num-workers", type=int, default=8)
+    parser.add_argument(       "--save-dir",    type=str, default=DEFAULT_SAVE_DIR)
+
     args = parser.parse_args()
     main(args)
